7bbce526967976146a5908800b1db8a0b5fc3b3f
Chethiya Kusal <chethiyakdis@gmail.com>
2024-06-05 22:30:13 +0530

    [BEA-96]Use Bing images as the daily image

---
+++ src/functions/utils.js
@@ -0,0 +1,45 @@
+export const getFromLocalStorage = arr => new Promise(resolve => {
+    //turn arr into array if it's not
+    if (!Array.isArray(arr)) arr = [arr];
+
+    // eslint-disable-next-line no-undef
+    if (!chrome?.storage) {
+        const res = {}
+        arr.forEach(key => {
+            res[key] = localStorage.getItem(key);
+        });
+        return resolve(res);
+    }
+
+    // eslint-disable-next-line no-undef
+    chrome?.storage?.local?.get(arr, res => resolve(res));
+})
+
+export const saveToLocalStorage = obj => new Promise(resolve => {
+    console.log(obj)
+    // eslint-disable-next-line no-undef
+    if (!chrome?.storage) {
+        Object.keys(obj).forEach(key => localStorage.setItem(key, JSON.stringify(obj[key])));
+        return resolve(true);
+    }
+
+    // eslint-disable-next-line no-undef
+    chrome?.storage?.local?.set(obj, res => resolve(true));
+})
+
+export const getBingImage = () => {
+
+    return new Promise(async resolve => {
+        const storageRes = await getFromLocalStorage("bingImage");
+        const today = new Date().toISOString().split('T')[0];
+        const bingImage = storageRes?.bingImage;
+        if (bingImage?.date === today) return resolve(bingImage?.imageData);
+
+        fetch('https://bing.biturl.top/').then(res => res.json()).then(data => {
+            const selectedImage = data.url
+            saveToLocalStorage({ bingImage: { date: today, imageData: selectedImage } });
+            resolve(selectedImage);
+
+        });
+    })
+}
